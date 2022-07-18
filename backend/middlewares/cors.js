12054aa92f27e8a64fc02798da9812766fab8b27
Miskevich Stanislav <miskevich95@yandex.ru>
2022-07-18 16:38:02 +0300

    update Cors.js +

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,10 +1,24 @@
 const cors = require('cors');
 
+const listUrl = () => {
+  const { NODE_ENV } = process.env;
+  let list = [];
+  if (!NODE_ENV) {
+    list = [
+      'https://localhost:3000',
+      'https://localhost:3001',
+    ];
+  } else {
+    list = [
+      'https://msprod.nomoredomains.xyz',
+      'http://msprod.nomoredomains.xyz',
+    ];
+  }
+  return list;
+};
+
 const allowedCors = {
-  origin: [
-    'https://msprod.nomoredomains.xyz/',
-    'http://msprod.nomoredomains.xyz/',
-  ],
+  origin: listUrl(),
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
